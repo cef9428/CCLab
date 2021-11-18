bfb60d6176bce439e4dcd37421b1dcf2f9495ccb
cef9428 <cef9428@nyu.edu>
2021-11-18 12:08:25 +0800

    added functions

--- interfaceArt/script.js
+++ interfaceArt/script.js
@@ -1,8 +1,7 @@
 let x = 0;
-let r = document.getElementById('red');
-let g = document.getElementById('green');
-let b = document.getElementById('blue');
 let start = false;
+let end = false;
+let grow = 1;
 function setup() {
   let canvas = createCanvas(800, 600);
   canvas.parent('here')
@@ -11,30 +10,57 @@ function setup() {
 }
 
 function draw() {
-  console.log(r.value,g.value,b.value);
-  if(start == true){
+  let degree = frameCount;
+  let pframecount = frameCount;
+  if(start == true && end == false){
     translate(width/2,height/2)
-    let degree = frameCount;
     let ang = radians(degree);
-    x = x + 1;
+    x = x + grow;
     rotate(sin(ang));
     noFill()
     rect(0,0,x,x)
-    if(x == 50){
-      stroke(r.value,g.value,b.value)
+    if(x<=25){
+      stroke(0);
+    }
+    else {
+      let r = document.getElementById('red');
+      let g = document.getElementById('green');
+      let b = document.getElementById('blue');
+      stroke(r.value,g.value,b.value);
     }
     if(x > height){
-      noLoop();
+      x = 0;
+      background(255);
     }
+    console.log(start,end);
   }
-
-}
-function say(fun){
-  console.log(fun);
-  if(fun = 1){
-    start = true
+  if(end == true && start ==  false){
+    grow = 0;
+    ang = 0
   }
   else{
+    grow = 1;
+    let fr = frameCount - pframecount;
+    degree = fr;
+    ang = radians(degree);
+  }
+}
+function say(fun){
+  if(fun > 0){
+    start = true;
+    end = false;
+  }
+  else if(fun == 0){
     start = false;
+  }
+}
+function stop(no){
+  if (no == 1){
+    end = true;
+    start = false;
+  }
+  else{
+    end = false;
+    start = true;
   }
 }
