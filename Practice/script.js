95b2106bcd263924a48e0c268f0e9b516557b5af
cef9428 <cef9428@nyu.edu>
2021-11-28 18:23:38 +0800

    binary

--- Practice/script.js
+++ Practice/script.js
@@ -1,54 +1,42 @@
+//based off https://editor.p5js.org/MOQN/sketches/c2oTHwVqd
+let char = [" ","I","1","0","o"];
+
 let img;
 let cam;
-let r;
-let g;
-let b;
-let a;
-function preload(){
-  img=createImage(640,480);
-}
-function setup(){
-  createCanvas(640,480);
+
+function setup() {
+  createCanvas(640, 480);
   cam = createCapture(VIDEO);
-  r = random(255);
-  g = random(255);
-  b = random(255);
-  //a = random(255);
+  cam.hide();
+  img = createImage(width, height);
 }
-function draw(){
-  let cutOff=document.getElementById('cutoff')
-  console.log(cutOff.value)
-  background('yellow');
 
-  img.loadPixels();
+function draw() {
+  background(0);
+
   cam.loadPixels();
+  img.loadPixels();
+  let gridSize = 10;
 
-  for(let y=0;y<480;y++){
-    for(let x=0;x<640;x++){
-      let index =(y*width+x)*4;
+  noStroke();
+  for (let y = 0; y < img.height; y += gridSize) {
+    for (let x = 0; x < img.width; x += gridSize) {
+      let index = (x + y * img.width) * 4;
 
-      let red = cam.pixels[index+0];
-      let green = cam.pixels[index+1];
-      let blue = cam.pixels[index+2];
-      // if(red<=(cutOff.value)||green<=(cutOff.value)||blue<=(cutOff.value)){
-      //   red=255;
-      //   green=255;
-      //   blue=255;
-      // }
-      // else if(red>=(cutOff.value)||green>=(cutOff.value)||blue>=(cutOff.value)){
-      //   red=255;
-      //   green=0;
-      //   blue=0;
-      // }
-      //let alpha = cam.pixels[index+3];
+      let red = cam.pixels[index + 0];
+      let green = cam.pixels[index + 1];
+      let blue = cam.pixels[index + 2];
 
+      let avg = (red + green + blue) / 3;
 
-      img.pixels[index+0]=red;
-      img.pixels[index+1]=green;
-      img.pixels[index+2]=blue;
-      img.pixels[index+3]=255;
+      let bCode = floor(
+        constrain(map(avg, 0, 255, 0, char.length), 0, char.length - 2)
+      );
+      // let a = sliderR.value;
+      // let c = sliderG.value;
+      // let d = sliderB.value;
+      fill(0,255,0);
+      text(char[bCode], x, y);
     }
   }
-  img.updatePixels();
-  image(img,0,0);
 }
