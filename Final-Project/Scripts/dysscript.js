63122f284224d94a789bf89471c549900774af47
cef9428 <cef9428@nyu.edu>
2021-12-07 12:59:35 +0800

    almost

--- Final-Project/Scripts/dysscript.js
+++ Final-Project/Scripts/dysscript.js
@@ -1,10 +1,17 @@
 let scra;
 let me;
 let newArray=[];
+let otherArray=[];
 let sca;
 let sa;
 let s;
 let l=0;
+let j=0;
+let dt;
+let dat;
+let d;
+let t;
+let da;
 function setup(){
 
 
@@ -14,6 +21,10 @@ function draw(){
   me = split(scra,"");
   sca=document.getElementById("fu").addEventListener('mouseover',myfunc);
   s=document.getElementById("fu").addEventListener("mouseout",myfunc2);
+  dt=document.getElementById("DT").textContent;
+  d = split(scra,"");
+  dat=document.getElementById("DT").addEventListener('mouseover',mydfunc);
+  t=document.getElementById("DT").addEventListener("mouseout",mydfunc2);
   // console.log(me);
   // let scram = split(scra," ");
   // console.log(scram);
@@ -35,7 +46,31 @@ function myfunc(){
 }
 function myfunc2(){
   //alert('help');
-    sa=document.getElementById("fu").textContent=scra.toString();
+  if(l>=1){
+  sa=document.getElementById("fu").textContent=scra.toString();  
+  }
+  // else{
+  //   console.log('no');
+  // }
+
+}
+function mydfunc(){
+  if(j<1){
+    j++;
+    for(let i=0;i<d.length;i++){
+    let f = floor(random(0,d.length));
+    otherArray.push(d[f]);
+    }
+    da=document.getElementById("DT").textContent=otherArray.join('');
+    console.log(dt);
+  }
+  else{
+    console.log('no');
+  }
+}
+function mydfunc2(){
+  //alert('help');
+    da=document.getElementById("DT").textContent=dt.toString();
   // else{
   //   console.log('no');
   // }
